3ea356a53669ed7793073805989f09ba380ae84c
Arturo-00 <acalveratonin@gmail.com>
2021-03-25 12:44:49 +0100

    Con volumenes

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #nginx -g 'daemon off;'
-certbot --nginx -n --agree-tos -m barbarosoft@gmail.com -d keypax.sytes.net
+certbot --nginx -n --agree-tos -m barbarosoft@gmail.com -d keypax-api.hopto.org
 trap : TERM INT
 tail -f /dev/null & wait
