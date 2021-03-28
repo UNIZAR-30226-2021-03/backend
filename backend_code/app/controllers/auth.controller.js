31466b891c4eaf57ef09ecf0a96455d08e840dd9
Andoni Salcedo <andonisalcedo@gmail.com>
2021-03-28 20:01:43 +0200

    send

--- backend_code/app/controllers/auth.controller.js
+++ backend_code/app/controllers/auth.controller.js
@@ -15,7 +15,7 @@ const SingUp = async(req,res) => {
         const userExist = await User.findUserByEmail(email);
 
         if(userExist){
-            return res.status(409).json({msg: "El usuario ya existe"});
+            return res.status(409).end();
         }
 
         const salt = await bcrypt.genSalt(20);
@@ -30,13 +30,13 @@ const SingUp = async(req,res) => {
 
         if (!sent){
             User.deleteUser(user._id);
-            return res.status(500);
+            return res.status(500).end();
         }
 
-        return res.status(200);
+        return res.status(200).end();
 
     }catch(err){
-        return res.status(500);
+        return res.status(500).send(err);
     }
 } 
 
@@ -49,21 +49,21 @@ const LogIn = async(req,res) => {
         const userExist = await User.findUserByEmail(email);
 
         if(!userExist){
-            return res.status(404);
+            return res.status(404).end();
         }
         
         const validPass = await bcrypt.compare(password,userExist.password);
 
         if (!validPass){
-            return res.status(401);
+            return res.status(401).end();
         }
 
         const token = jwt.sign({_id: userExist._id},config.ACCESS_TOKEN,{expiresIn: 3600});
 
-        res.status(200).send({accessToken:token});
+        return res.status(200).send({accessToken:token});
 
     }catch(err){
-        return res.status(500);
+        return res.status(500).end();
     } 
 }
 
@@ -73,9 +73,9 @@ const Verify = async(req,res) => {
     const verified = jwt.verify(token,config.VERIFICATION_TOKEN);
     if(verified){
         User.updateVerify(verified._id);
-        return res.status(200);
+        return res.status(200).end();
     }
-    return res.status(401);
+    return res.status(401).end();
     
 } 
 module.exports = {SingUp,LogIn,Verify}
