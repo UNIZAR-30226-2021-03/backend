033db71a726959f382dbf48bea3ac3c2996a0cc5
Arturo-00 <acalveratonin@gmail.com>
2021-05-18 13:00:58 +0200

    Otras pruebas

--- backend_code/app/middleware/auth.middleware.js
+++ backend_code/app/middleware/auth.middleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const config = require('../config')
 
 const authValidation = (req,res,next) => {
+    console.log("ANTES DE LA FUNCIÓN",req.header('accessToken'))
     try{
         const token = req.header('accessToken')
         if(!token){
@@ -11,13 +12,14 @@ const authValidation = (req,res,next) => {
         const verified = jwt.verify(token,config.ACCESS_TOKEN)
         if (verified){
             req.token = verified
+            console.log("CORRECTO",req.header('accessToken'))
             next()
         }else{
-            console.log("")
+            console.log("POR EL ELSE DE VERIFY",req.header('accessToken'))
             return res.status(401).send({})
         }
     }catch(err){
-        console.log(req.header('accessToken'));
+        console.log("POR EL CATCH",req.header('accessToken'))
         console.log(err);
         return res.status(401).send(err)
     }
